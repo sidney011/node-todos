acd7a8a9838b62d4f0dd0f0454970882d28cff96
Sidney Wimart <sidney@sidneys-macbook-pro.home>
2019-03-15 23:00:45 +0100

    Add tests for PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -10,7 +10,9 @@ const todos = [{
     text: 'First test todo'
 }, {
     _id: new ObjectID(),
-    text: 'Second test todo'
+    text: 'Second test todo',
+    completed: true,
+    completedAt: 333
 }];
 beforeEach((done) => {
     Todo.remove({}).then(() => {
@@ -139,4 +141,46 @@ describe('DELETE todos/:id', () => {
         .end(done)
 
     });
-})
+});
+
+describe('PATCH todos/:id', () => {
+    it('should update the todo', (done) => {
+        var hexId = todos[0]._id.toHexString();
+        var newUpdate = {text: "Updated", completed: true}
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .send(newUpdate)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(newUpdate.text);
+                expect(res.body.todo.completed).toBe(newUpdate.completed);
+                expect(res.body.todo.completedAt).toBeA('number');
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                done();
+            });
+    });
+
+    it('should clear completedAt when todo is not completed', (done) => {
+        var hexId = todos[0]._id.toHexString();
+        var newUpdate = {text: "Updated", completed: false}
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .send(newUpdate)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(newUpdate.text);
+                expect(res.body.todo.completed).toBe(newUpdate.completed);
+                expect(res.body.todo.completedAt).toNotExist();
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                done();
+            });
+    });
+});
